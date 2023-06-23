34c2c543702edf0733779f011dde31d7cf094341
Tomáš <50266962+TypoCo@users.noreply.github.com>
2023-06-23 11:32:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Moje obľúbené jedlo</h1>
+      <ul>
+        <li>Zemiakové placky</li>
+        <li>Zemiakové placky</li>
+        <li>Zemiakové placky</li>
+        <li>Zemiakové placky</li>
+      </ul>
+    </div>
+  );
+}
